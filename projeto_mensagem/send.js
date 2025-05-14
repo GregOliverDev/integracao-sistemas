cf45d0eaae6e342a6a72d00d1b131f23c841b0da
Gregory de Oliveira <gregoliverdev@gmail.com>
2025-05-14 20:11:23 -0300

    alter projeto_mensagem

---
+++ projeto_mensagem/send.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+    if (error0) throw error0;
+    connection.createChannel((error1, channel) => {
+        if (error1) throw error1;
+        
+        const queue = 'q1';
+        const message = 'Hello world';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(message));
+        console.log(" [x] Sent menssage: %s", message);
+    });
+});
